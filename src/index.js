d8a638e10cd92a07b017d111f536dbf80ecab87b
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-09-08 00:15:52 -0300

    bg

--- src/index.js
+++ src/index.js
@@ -9,9 +9,13 @@ import { AppProvider } from './context/AppContext';
 import { Header } from './Components/Header/Header';
 import { Footer } from './Components/Footer/Footer';
 
+import './index.css';
+import { BackgroundCss } from './Components/background/BackgroundCss';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BackgroundCss />
     <BrowserRouter>
       <AppProvider>
         <Header />
