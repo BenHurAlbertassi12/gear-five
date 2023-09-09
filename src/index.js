bcb59527284302e6c087d7eda9fb28ab6ea818dc
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-09-08 22:11:23 -0300

    clean

--- src/index.js
+++ src/index.js
@@ -10,12 +10,10 @@ import { Header } from './Components/Header/Header';
 import { Footer } from './Components/Footer/Footer';
 
 import './index.css';
-// import { BackgroundCss } from './Components/background/BackgroundCss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <BackgroundCss /> */}
     <BrowserRouter>
       <AppProvider>
         <Header />
--- src/templates/Home/TitleHome/TitleHome.jsx
+++ src/templates/Home/TitleHome/TitleHome.jsx
@@ -1,10 +1,15 @@
 import './TitleHome.css';
 
+import logo from '../../../assets/Logo G5 1500x1500 original color.png';
+
 export const TitleHome = () => {
   return (
-    <div className="div-intro">
-      <h1>Gear-5 Desenvolvimento</h1>
-      <h2>A Quinta Marcha do seu negócio</h2>
+    <div>
+      <img src={logo} className="img-logo-title" />
+      <div className="div-intro">
+        <h1>Gear-5 Desenvolvimento</h1>
+        <h2>A Quinta Marcha do seu negócio</h2>
+      </div>
     </div>
   );
 };
