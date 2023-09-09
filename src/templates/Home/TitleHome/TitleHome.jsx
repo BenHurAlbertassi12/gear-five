e113dec273f58a6c46600df0a992467f5878b7d9
BenHur Albertassi da Silva <albertassibenhur@gmail.com>
2023-09-08 23:42:28 -0300

    css bonito

--- src/templates/Home/TitleHome/TitleHome.jsx
+++ src/templates/Home/TitleHome/TitleHome.jsx
@@ -1,15 +1,17 @@
 import './TitleHome.css';
+import './h3-sub-title.css';
+import './logo.css';
 
 import logo from '../../../assets/Logo G5 1500x1500 original color.png';
 
 export const TitleHome = () => {
   return (
     <div>
-      <img src={logo} className="img-logo-title" />
       <div className="div-intro">
-        <h1>Gear-5 Desenvolvimento</h1>
-        <h3>A Quinta Marcha do seu negócio</h3>
+        <h1 className="text-focus-in">Gear-5 Desenvolvimento</h1>
+        <h2 className="h3-sub-title">A Quinta Marcha do seu negócio</h2>
       </div>
+      <img src={logo} className="img-logo-title scale-up-center" />
     </div>
   );
 };
